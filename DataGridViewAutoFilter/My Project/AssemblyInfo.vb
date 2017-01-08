b855dfa4498bedcf93d036706d57a214cceba8bb
Joe Mireles <bs.GitHub@burnsoft.net>
2017-01-08 13:40:03 -0500

    New Project to Git

---
+++ DataGridViewAutoFilter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BS DataGridViewAutoFilter .NET 4.5")> 
+<Assembly: AssemblyDescription("Add On to the DataGrid to filter out the results")> 
+<Assembly: AssemblyCompany("BurnSoft www.burnsoft.net")> 
+<Assembly: AssemblyProduct("BS DataGridViewAutoFilter")> 
+<Assembly: AssemblyCopyright("Copyright © BurnSoft 2010-2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("216764f3-4ca1-49d1-a06c-8ff82f3629ee")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.5.1.*")> 
+<Assembly: AssemblyFileVersion("4.5.1.15736")> 
