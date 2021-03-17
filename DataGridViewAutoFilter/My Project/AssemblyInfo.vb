15c91f2771aa41cb6cf130c16a6db2f63028f9c0
Joe M <bs.github@burnsoft.net>
2021-03-17 18:23:18 -0400

    Updated version and .net framework to version 4.7.2

--- DataGridViewAutoFilter/My Project/AssemblyInfo.vb
+++ DataGridViewAutoFilter/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.5.0.*")>
-<Assembly: AssemblyFileVersion("4.5.0.15736")>
+<Assembly: AssemblyVersion("4.7.2.*")>
+<Assembly: AssemblyFileVersion("4.7.2.1")>
